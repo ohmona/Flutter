3fd8fda9f0efb7af331c9a94b9781ba1e46aac66
ohmona <66560014+ohmona@users.noreply.github.com>
2023-04-15 00:07:26 +0200

    Update main.dart

--- toonflix/main.dart
+++ toonflix/main.dart
@@ -1,1 +1,54 @@
+import 'package:flutter/material.dart';
 
+void main() {
+  runApp(App());
+}
+
+class Player {
+  String name;
+  Player({required this.name});
+}
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+     return MaterialApp(
+       home: Scaffold(
+         backgroundColor: Color(0xFF181818),
+         body: Padding(
+           padding: EdgeInsets.symmetric(horizontal: 10),
+           child: Column(
+             children: [
+               SizedBox(
+                 height: 20,
+               ),
+               Row(
+                 mainAxisAlignment: MainAxisAlignment.end,
+                 children: [
+                   Column(
+                     crossAxisAlignment: CrossAxisAlignment.end,
+                     children: [
+                       Text('Hey, Selena',
+                         style: TextStyle(
+                           color: Colors.white,
+                           fontSize: 25,
+                           fontWeight: FontWeight.w800,
+                         ),
+                       ),
+                       Text('Welcome back',
+                         style: TextStyle(
+                           color: Colors.white.withOpacity(0.8),
+                           fontSize: 12,
+                         ),
+                       ),
+                     ],
+                   )
+                 ],
+               )
+             ],
+           ),
+         )
+       ),
+     );
+  }
+}
